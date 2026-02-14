50451ae4fd78465a76ad0a01f44d4729cd279c4e
taroot <tarowtb@pm.me>
2026-02-14 16:08:45 +0100

    docker hook complete

--- conf/hooks.sh
+++ conf/hooks.sh
@@ -1,27 +1,50 @@
 #!/bin/bash
 
+
+
 docker_manager() {
         local action="$1"
+        local state_file="/tmp/borgetmenot_docker.state"
+
+        if ! command -v docker &> /dev/null; then
+                log WARN "Docker command not found."
+        return 0
+        fi
 
         if [[ "$action" == "stop" ]]; then
-                export DOCKER_ID_NAME="$(docker ps --format "{{.ID}}:{{.Names}}")"
+                local docker_id_name=$(docker ps --format "{{.ID}}:{{.Names}}")
+                if [[ -z "$docker_id_name" ]]; then
+                        log INFO "No running Docker containers to stop."
+                        touch "$state_file"
+                        return 0
+                fi
+
                 local sentence="Stopping"
-        else
+                echo "$docker_id_name" > "$state_file"
+
+        elif [[ "$action" == "start" ]]; then
+                if [[ ! -f "$state_file" ]]; then
+                        log WARN "No state file found at $state_file. No Docker container to start."
+                        return 0
+                fi
+
                 local sentence="Starting"
         fi
 
-        if [[ -n "$DOCKER_ID_NAME" ]]; then
-                log INFO "$sentence Docker container..."
+        while IFS= read -r line; do
+                [[ -z "$line" ]] && continue
+                local d_id=$(echo "$line" | cut -d: -f1)
+                local d_name=$(echo "$line" | cut -d: -f2)
 
-                for d in $DOCKER_ID_NAME; do
-                        local d_id=$(echo $d | cut -d: -f1)
-                        local d_name=$(echo $d | cut -d: -f2)
+                log INFO "$sentence Docker container: $d_name ($d_id)..."
+                docker "$action" "$d_id" > /dev/null
+        done < "$state_file"
 
-                        log INFO "$d_name ($d_id)..."
-                        docker "$action" "$d_id"
-                done
+        if [[ "$action" == "start" ]]; then
+                rm "$state_file"
         fi
 }
+
 
 init_saver() {
         log INFO "Saving init system & tools..."
