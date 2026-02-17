4e6b83e2196dddbfaccf4e6ce3be9acf63004f6f
taroot <taro.wtb@pm.me>
2026-02-17 23:16:27 +0100

    config repo and conf file

--- wasurenagusa.sh
+++ wasurenagusa.sh
@@ -5,13 +5,20 @@ set -o pipefail
 set -E
 
 
-read -e -p "Enter config file path: " CONFIG_FILE
+CONFIG_FILE="/usr/local/bin/borgetmenot/conf/borgetmenot_$(hostname).conf"
+
+read -r -p "Enter config file (y/N): " CHANGE_CONFIG
+if [[ "$CHANGE_CONFIG" =~ ^[yY]$ ]]; then
+	read -e -p "Enter config file path: " NEW_CONFIG
+	export CONFIG_FILE="$NEW_CONFIG"
+fi
 
 if [[ ! -f "$CONFIG_FILE" ]]; then
 	echo "Error: Configuration file '$CONFIG_FILE' not found."
 	exit 1
 fi
 
+echo ""
 
 echo "Loading configuration from $CONFIG_FILE..."
 source "$CONFIG_FILE"
@@ -25,6 +32,16 @@ export BORG_PASSCOMMAND="cat $BORG_PASSFILE"
 
 echo "Done."
 
+echo ""
+
+echo $REPO_PATH
+read -r -p "Enter borg repo (y/N): " CHANGE_REPO
+if [[ "$CHANGE_REPO" =~ ^[Yy]$ ]]; then
+	read -r -p "Enter repo path: " NEW_PATH
+	export BORG_REPO="$NEW_PATH"
+fi
+
+echo ""
 
 echo "Enter borg arguments"
 read -e -p "borg " BORG_ARGS
